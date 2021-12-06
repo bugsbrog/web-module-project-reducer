a1e98687126812a40baa065de9596ece1de69e76
bugsbrog <bugsbrog@gmail.com>
2021-12-06 13:51:00 -0700

    Removed/commented out the addOne

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
 import React, { useReducer } from 'react';
 import reducer, { initialState } from './../reducers/index';
 import './App.css';
-import { addOne, applyNumber } from '../actions/index';
+import { applyNumber } from '../actions/index';
 
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const handleAdd = (num) => {
-    dispatch(addOne(num))
-  }
+  // const handleAdd = (num) => {
+  //   dispatch(addOne(num))
+  // }
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
@@ -34,7 +34,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton onClick={() => handleAdd(1)} value={1}/>
+              <CalcButton value={1}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
