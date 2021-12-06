017e487c734c020011f87e365311752babc68233
bugsbrog <bugsbrog@gmail.com>
2021-12-06 13:32:53 -0700

    Imported addOne into App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useReducer } from 'react';
 import reducer, { initialState } from './../reducers/index';
 import './App.css';
+import { addOne } from '../actions/index';
 
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
